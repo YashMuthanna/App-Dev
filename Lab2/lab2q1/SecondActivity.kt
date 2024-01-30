f0d1aa6fead3d53780cf5baa54da6dafd09739b3
YashMuthanna <49674112+YashMuthanna@users.noreply.github.com>
2024-01-30 23:50:07 +0530

    Create SecondActivity.kt

---
+++ Lab2/lab2q1/SecondActivity.kt
@@ -0,0 +1,40 @@
+package com.example.app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class SecondActivity : AppCompatActivity() {
+
+    val TAG = "Second Activity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+}
